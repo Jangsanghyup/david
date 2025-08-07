c3eba0feb6fd79a304e7080640c4118fda5aa5ae
sanghyeob <sanghyeob93@naver.com>
2025-08-07 17:33:22 +0900

    commit

--- origin/app.py
+++ origin/app.py
@@ -3,6 +3,20 @@ import socket
 
 app = Flask(__name__)
 
+# if __debug__:
+#     print("Debug mode is ON")
+# else:
+#     print("Debug mode is OFF")
+
+# __debug__ 플래그란?
+# Python에는 기본적으로 __debug__라는 내장 상수가 있으며, 이는 다음과 같이 작동합니다:
+
+# 기본적으로 __debug__ == True입니다.
+
+# Python을 최적화 모드(-O) 로 실행하면 __debug__ == False가 됩니다.
+
+# python -O app.py
+
 @app.route('/')
 def home():
     if app.debug:
