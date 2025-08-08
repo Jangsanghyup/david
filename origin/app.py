7719af47dd13d8ff210d40bd9ca5b326abbbfb5e
sanghyeob <sanghyeob93@naver.com>
2025-08-08 15:27:13 +0900

    commit

--- origin/app.py
+++ origin/app.py
@@ -19,14 +19,14 @@ def menu():
 def test1():
     return render_template('test1.html')
 
-@app.route("/test2")
-def test2():
-    return render_template('test2.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
 
 
+
+# 협업 중 --force는 되도록 피하고, 충돌은 pull → merge → commit → push 방식으로 해결
+
+# 만약 --force를 써야 한다면 반드시 동료에게 공유하고 동기화할 것
 
 
     # if __debug__:
