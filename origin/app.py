9d020e40e55ef5dd2a695723f4f8802d4834e3ef
sanghyeob <sanghyeob93@naver.com>
2025-08-13 17:10:04 +0900

    Add /test2 route

--- origin/app.py
+++ origin/app.py
@@ -3,6 +3,7 @@ import socket
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     if app.debug:
@@ -15,9 +16,14 @@ def home():
 def menu():
     return render_template('menu.html')
 
-@app.route('/test1')
+@app.route("/test1")
 def test1():
     return render_template('test1.html')
+
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
